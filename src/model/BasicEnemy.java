4c53353e80fce55e29defe30211b3b028f458e63
mjowned <mj65@live.nl>
2015-10-01 12:32:38 +0200

    Trail fixes, also got something new after trial and error (currently commented).

--- src/model/BasicEnemy.java
+++ src/model/BasicEnemy.java
@@ -15,12 +15,16 @@ public class BasicEnemy extends GameObject {
 
 	private Handler handler;
 	
+	private int angle = 0;
+	private int angle2 = 0;
+	private int angleVel = 5;
+	
 	public BasicEnemy(int x, int y, ID id, Handler handler) {
 		super(x, y, id);
 		this.img = new ImageIcon(this.getClass().getResource("../images/chaser.png")).getImage();
 		
-		velX = 1;
-		velY = 1;
+		velX = 3;
+		velY = 3;
 		
 		width = 48;
 		height = 48;
@@ -36,6 +40,17 @@ public class BasicEnemy extends GameObject {
 		x += velX;
 		y += velY;
 		
+		angle += angleVel;
+		angle2 += angleVel;
+		
+		if(angle >= 360) {
+			angle = 0;
+		}
+		
+		if(angle2 >= 360) {
+			angle2 = 0;
+		}
+		
 		if(y <= 0 || y >= Game.HEIGHT - 48) {
 			velY *= -1;
 		}
@@ -45,12 +60,13 @@ public class BasicEnemy extends GameObject {
 		}
 		
 //		handler.addObject(new Trail(x, y, ID.Trail, Color.red, width, height, 0.03f, handler));
-		handler.addObject(new Trail(x, y, ID.Trail, Color.blue, (int) (width * 0.25), (int) (height * 0.25), 0.03f, handler));
+		
+		//best I could manage for now
 		handler.addObject(new Trail((int) calculatePlacementTrailX(), (int) calculatePlacementTrailY(), ID.Trail, Color.orange, (int) (width * 0.25), (int) (height * 0.25), 0.03f, handler));
-		System.out.println("VelX: "+ velX);
-		System.out.println("VelY: "+ velY);
-		System.out.println("X: "+(int) (x-calculatePlacementTrailX()));
-		System.out.println("Y: "+(int) (y-calculatePlacementTrailY()));
+		
+		//cool result, very interesting. Not yet working as intended, but still =P
+//		handler.addObject(new Trail((int) calculateWavePattern1X(), (int) calculateWavePattern1Y(), ID.Trail, Color.orange, (int) (width * 0.1), (int) (height * 0.1), 0.03f, handler));
+//		handler.addObject(new Trail((int) calculateWavePattern2X(), (int) calculateWavePattern2Y(), ID.Trail, Color.orange, (int) (width * 0.1), (int) (height * 0.1), 0.03f, handler));
 	}
 	
 	/**
@@ -65,14 +81,32 @@ public class BasicEnemy extends GameObject {
 		float y = r*sin(t) + k;
 	 */
 	protected double calculatePlacementTrailX(){
-
-		double radians = Math.atan2(velY, velX) + Math.toRadians(0); //rotation depending on graphics formula
-		return (width*0.5)*Math.sin(Math.toDegrees(radians)) + getCenterObjectX(x); //
+		double radians = Math.atan2(velY, velX) + Math.toRadians(180); //rotation depending on graphics formula
+//		return (width*0.5)*Math.sin(Math.toDegrees(radians)) + getCenterObjectX(x);
+		return ((width*0.5)-20)*Math.cos(radians) + getCenterObjectX(x);
+//		return ((width*0.5))*Math.cos(Math.toRadians(angle)) + getCenterObjectX(x);
 	}
 	
 	protected double calculatePlacementTrailY(){
-		double radians = Math.atan2(velY, velX) + Math.toRadians(0); //rotation depending on graphics formula
-		return (height*0.5)*Math.cos(Math.toDegrees(radians)) + getCenterObjectX(y);
+		double radians = Math.atan2(velY, velX) + Math.toRadians(180); //rotation depending on graphics formula
+		return ((height*0.5)-20)*Math.cos(Math.toDegrees(radians)) + getCenterObjectX(y);
+//		return ((height*0.5))*Math.sin(Math.toRadians(angle)) + getCenterObjectX(y);
+	}
+	
+	protected double calculateWavePattern1X(){
+		return ((width*0.5))*Math.cos(Math.toRadians(angle)) + getCenterObjectX(x);
+	}
+	
+	protected double calculateWavePattern1Y(){
+		return ((height*0.5))*Math.sin(Math.toRadians(angle)) + getCenterObjectX(y);
+	}
+	
+	protected double calculateWavePattern2X(){
+		return ((width*0.5))*Math.sin(Math.toRadians(angle2)) + getCenterObjectX(x);
+	}
+	
+	protected double calculateWavePattern2Y(){
+		return ((height*0.5))*Math.cos(Math.toRadians(angle2)) + getCenterObjectX(y);
 	}
 	
 	public void render(Graphics2D g2d, ImageObserver o) {
