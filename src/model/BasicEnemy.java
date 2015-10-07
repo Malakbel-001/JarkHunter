5dba3dc6964092d7351e26837e75d51cc21f31e7
mjowned <mj65@live.nl>
2015-10-07 21:22:16 +0200

    refactor

--- src/model/BasicEnemy.java
+++ src/model/BasicEnemy.java
@@ -59,26 +59,6 @@ public class BasicEnemy extends GameObject {
 		this.collidableContainer.remove(collidableBehaviour);
 	}
 	
-//	public void render(Graphics2D g2d) {
-//		g2d.setColor(Color.red);
-//		g2d.drawRect((int) x,(int) y, width, height);
-//		
-//		//get rotation angle depending on velocity
-//		double radians = Math.atan2(moveBehaviour.getVelY(), moveBehaviour.getVelX()) + Math.toRadians(90);
-//		
-//		//remember old transform to revert rotation on graphics
-//		AffineTransform originalTransform = g2d.getTransform();
-//		
-//		//rotate all the following graphic draw actions
-//		g2d.rotate(radians, getCenterObjectX((int) x), getCenterObjectY((int) y)); //make sure to give the center point of the image!!!
-//		
-//		//draw the image, while being rotated
-//		g2d.drawImage(img, (int) x, (int) y, null);
-//		
-//		//revert transform / rotation on graphics, so OTHER GAMEOBJECTS that get RENDERED, do NOT get rotated!
-//		g2d.setTransform(originalTransform);
-//	}
-	
 	public void setVelX(int velX){
 		this.velX = velX;
 	}
